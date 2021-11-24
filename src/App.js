58d582ad90da12f77bde9420802edcb14ad34739
Blazing-Storm <mayankgoyalstar@gmail.com>
2021-11-24 15:57:50 +0530

    fed st3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Square from "./components/Square";
+
+const clearState = ["", "", "", "", "", "", "", "", "", ""];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [gameState, updateGameState] = useState(clearState)
+    const [isXChance, updateIsXChance] = useState(false)
+
+    const onUserClicked = (index) => {
+        let strings = Array.from(gameState);
+        if (strings[index])
+            return;
+        strings[index] = isXChance ? "X" : "O";
+        updateIsXChance(!isXChance)
+        updateGameState(strings)
+    }
+
+    const clearGame = () => {
+        updateGameState(clearState)
+    }
+    useEffect(() => {
+        let winner = checkWinner();
+        if (winner) {
+            clearGame();
+            alert(`Ta da! Player ${winner} won the Game!`)
+        }
+    }, [gameState])
+
+    const checkWinner = () => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        console.log('Class: App, Function: checkWinner ==', gameState[0], gameState[1], gameState[2]);
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+                return gameState[a];
+            }
+        }
+        return null;
+    }
+
+    return (
+        <div className="app-header">
+            <p className="heading-text">React Tic Tac Toe</p>
+            <div className="row jc-center">
+                <Square className="b-bottom-right" onClick={() => onUserClicked(0)} state={gameState[0]}/>
+                <Square className="b-bottom-right" onClick={() => onUserClicked(1)} state={gameState[1]}/>
+                <Square className="b-bottom" onClick={() => onUserClicked(2)} state={gameState[2]}/>
+            </div>
+            <div className="row jc-center">
+                <Square className="b-bottom-right" onClick={() => onUserClicked(3)} state={gameState[3]}/>
+                <Square className="b-bottom-right" onClick={() => onUserClicked(4)} state={gameState[4]}/>
+                <Square className="b-bottom" onClick={() => onUserClicked(5)} state={gameState[5]}/>
+            </div>
+            <div className="row jc-center">
+                <Square className="b-right" onClick={() => onUserClicked(6)} state={gameState[6]}/>
+                <Square className="b-right" onClick={() => onUserClicked(7)} state={gameState[7]}/>
+                <Square onClick={() => onUserClicked(8)} state={gameState[8]}/>
+            </div>
+            <button className="reset-button" onClick={clearGame}>Reset Game</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Square = (props) => {
+    const classes = (props.className ? `${props.className} square` : `square`)
+    return (
+        <span
+            className={classes + (props.state === "X" ? ` fc-black` : ` fc-white`)}
+            onClick={() => props.onClick(props.index)}>
+           {props.state}
+        </span>
+    )
+}
+export default Square
